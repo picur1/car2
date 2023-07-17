7cba37cf06d1d7fa1072324bd996cc74cb540e74
picur1 <133989406+picur1@users.noreply.github.com>
2023-07-17 15:23:42 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+radio.onReceivedString(function (receivedString) {
+    if (true) {
+    	
+    } else {
+    	
+    }
+})
+radio.setGroup(1)
+RingbitCar.init_wheel(AnalogPin.P1, AnalogPin.P2)
+basic.forever(function () {
+	
+})
